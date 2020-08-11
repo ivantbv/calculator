37c0896e858d64ed0139e50a521b2a4b39e9d536
Ivan <ivantbv@gmail.com>
2020-08-11 15:34:21 +0500

    each number corresponds to its value when clicked and is displayed

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function factorial(n) {
 // 	return parsed;
 // }
 
-//Create a new function operate that takes an operator and 2 
+//function operate that takes an operator and 2 
 //numbers and then calls one of the above functions on the numbers.
 
 
@@ -54,3 +54,17 @@ function operate(x, oper, y) {
 		return power(x, y);
 	}
 }
+
+//Create the functions that populate the display when you click the number buttons… you 
+//should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
+const calcBtns = document.querySelectorAll('.nmb');
+let dispCont = document.querySelector('#display')
+let displayValues;
+calcBtns.forEach(btn => btn.addEventListener('click', (e) => {
+	
+	displayValues = dispCont.value += e.target.value;
+	console.log(displayValues);
+}));
+
+
