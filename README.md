cd2a104368cd606a8984abf17cbe437fdf1971b1
Ivan <ivantbv@gmail.com>
2020-10-23 15:46:43 +0500

    added a module and returned only the needed public methods.Cleaned up the code, removed console logs and comments.  updated the readme

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 This is a simple browser calculator made in vanilla JavaScript. Supports multiple equations.
 
+# Under the hood and code structure
+
+I used Factory Functions and the Module Pattern. The whole code is wrapped in a Module which has private and public methods, and the ladder gets returned and called;
+
+Practiced DOM manipulation and key events. Especially challenging was implementing the shift key in combination with other keys for the operators.
+
+# UI and styling
+
+Simple UI with the calculator centered. Made use of the css grid with grid-template-areas to order the calculator's buttons
+
+# Functionality
 
 -custom made function for the operations (no eval() used!)
 -full keyboard support: type operators and operands just like
@@ -14,3 +25,6 @@ you would as usual, using the numbers over the QWERTY, OR/AND the numpad. Press 
     - n (factorial)
 
     Try it live here:  ivantbv.github.io/calculator/ 
+
+# Things to improve
+Implement media queries in the CSS file and make it mobile friendly
--- script.js
+++ script.js
@@ -1,132 +1,76 @@
-function add (...x) {
-	return result = x.reduce((a,b) => a += b);		
-}
-function subtract (...x) {
-	return result = x.reduce((a,b) => a -= b);
-}
-function multiply (...x) {
-	return result = x.reduce((a,b) => a *= b);
-}
-function divide(...x) {
-    return result = x.reduce((a,b) => a /= b);
-}
-function power(x,y) {
-	return x ** y;
-}
-function factorial(n) {
-	return n !== 0 ? n * factorial(n - 1) : 1;
-}
-function roundDecimals(n) {
-	Math.round(n * 100000) / 100000
-}
-
-function negate(n) {
-	return n = -n;
-}
-//√
-// function squareRoot(n) {
-// 	return Math.sqrt(n);
-// }
-
-//a = ~a + 1; - use the bitwises operator to implement a negate function ±
-
+const Calculator = (() => {
 const calcBtns = document.querySelectorAll('.nmb');
-const operBtns = document.querySelectorAll('.oper')
 const dispCont = document.querySelector('#display');
 const histDispCont = document.querySelector('#disp-history');
-let paraForHist = document.createElement('p');
-const inputFromDisp = dispCont.value;
 const equalSign = document.querySelector('.operator-equals')
-
 const allOperators = document.querySelectorAll('.oper');
-// const plus = document.querySelector('.operator-add').value;
 const factorialOper = document.querySelector('.operator-factorial');
 const decimalPoint = document.querySelector('.point')
 const backspace = document.querySelector('.backspace');
-
 const plusSign = document.querySelector('.operator-add');
 const divideSign = document.querySelector('.operator-divide');
 const multiplySign = document.querySelector('.operator-multiply');
-
 const subtractSign = document.querySelector('.operator-subtract');
 const powerSign = document.querySelector('.operator-power');
- 
-function operate(x, oper, y) {
-	console.log(x, oper, y)
-	if (oper === '+') {
-		return add(x, y);
-	} else if (oper === '/' || oper === '÷') {
-		if (y === 0) {
-			return 'Can\'t divide by 0'
-		} else {
-		return divide(x, y);
-	}
-	} else if (oper === '*' || oper === 'x') {
-		return multiply(x, y);
-	} else if (oper === '-') {
-		return subtract(x, y);
-	} else if (oper === '^' || oper === 'x^') {
-		return power(x, y);
-	}
-}
-
+const CE = document.querySelector('.clear');
 let shiftPressed = false;
 let displayValues;
 let numberValues = [];
 let operatorValues = [];
-let afterOperatorNumbers = [];
 
-
-calcBtns.forEach(btn => btn.addEventListener('click', (e) => {
-	
-	displayValues = dispCont.value += e.target.value;
-
-	numberValues = dispCont.value
-
-//	numberValues = displayValues;
-
-}));
-
-function operatorFunc(e) {
-	if (numberValues != '' && histDispCont.value != '') {
-		//histDispCont.value.substring(histDispCont.value.lastIndexOf(e.target.value) + 1);
-		if (histDispCont.value.includes('fact')) {
+//privates
+function keyDownEventsWithShift(e) {
+	if (e.which == 16) {shiftPressed = true;} 
+	if (e.which == 56 && shiftPressed || e.which == 106) {
+		if (numberValues != null && histDispCont.value != null) {
+			equalFunc(e);		
+		}
+		if (dispCont.value == null || histDispCont.value == null) {
+			
+			dispCont.value = '';
 			histDispCont.value = '';
 		}
-		equalFunc(e);	
-	} 
-
-	if (dispCont.value == null || histDispCont.value == null) {
-		e.target.value = ''
+		histDispCont.value = dispCont.value + multiplySign.value;
+		operatorValues += multiplySign.value;
 		dispCont.value = '';
-		histDispCont.value = '';
+		equalSign.disabled = false;
+	} else if (e.which == 54 && shiftPressed) {
+		if (numberValues != null && histDispCont.value != null) {
+
+			if (histDispCont.value.includes('fact')) {
+				histDispCont.value = '';
+			}
+			equalFunc(e);		
+		} 
+	
+		if (dispCont.value == null || histDispCont.value == null) {
+			
+			dispCont.value = '';
+			histDispCont.value = '';
+		}
+
+		histDispCont.value = dispCont.value + powerSign.value;
+		operatorValues += powerSign.value;
+		dispCont.value = '';
+		equalSign.disabled = false;
+	} else if (e.which == 61 || e.which == 187 && shiftPressed) {
+		if (numberValues != '' && histDispCont.value != '') {
+			equalFunc(e);		
+		} 
+	
+		if (dispCont.value == null || histDispCont.value == null) {
+			dispCont.value = '';
+			histDispCont.value = '';
+		}
+		histDispCont.value = `${dispCont.value}${plusSign.value}`;
+		operatorValues = `${operatorValues}${plusSign.value}` 
+		dispCont.value = '';
+		equalSign.disabled = false;
 	}
-
-	if (dispCont.value.indexOf('NaN') > -1 || histDispCont.value.indexOf('NaN') > -1){
-		dispCont.value = '';
-	}
-	// histDispCont.value += dispCont.value
-	// dispCont.value = ''
-
-		histDispCont.value = `${dispCont.value}${e.target.value}`;
-		console.log(numberValues);
-		operatorValues = `${operatorValues}${e.target.value}` 
-		dispCont.value = '';
 }
 
- allOperators.forEach(btn => btn.addEventListener('click', (e) => {
-	operatorFunc(e)
-	equalSign.disabled = false;	
-}));
-
-function domNode(nodeType, text) {
-    var node = document.createElement(nodeType);
-    if (text) node.appendChild(document.createTextNode(text));
-    return node;
-    }
-
- document.addEventListener('keydown', (e) => {
-	 e.preventDefault();
+function keyDownEvents(e) {
+	e.preventDefault();
 	if (e.which == 49 || e.which == 97) {
 		dispCont.value += 1;
 		numberValues = displayValues;
@@ -188,20 +132,12 @@ function domNode(nodeType, text) {
 		if (splitted.includes('N', 'A')) {
 				 	dispCont.value = '';
 				}
-
-				//let arryed = Array.from(histDispCont.value);
-				// let splitted = histDispCont.value.split('');
-				// if (splitted.includes('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')) {
-				// 	histDispCont.value = '';
-				// }
-
 				equalFunc(e);
 
 	} else if (e.which == 107) {
 		if (numberValues != null && histDispCont.value != null) {
 			equalFunc(e);		
 		} 
-	
 		if (dispCont.value == null || histDispCont.value == null) {
 			
 			dispCont.value = '';
@@ -224,7 +160,6 @@ function domNode(nodeType, text) {
 			dispCont.value = '';
 			histDispCont.value = '';
 		}
-
 		histDispCont.value = dispCont.value + subtractSign.value;
 		operatorValues += subtractSign.value;
 		dispCont.value = '';
@@ -238,73 +173,13 @@ function domNode(nodeType, text) {
 			dispCont.value = '';
 			histDispCont.value = '';
 		}
-
-
 		histDispCont.value = dispCont.value + divideSign.value;
 		operatorValues += divideSign.value;
 		dispCont.value = '';
 		equalSign.disabled = false;
 	}
-	console.log(e.which);
-	
 	numberValues = dispCont.value;
-})
-document.addEventListener('keydown', function(e) {
-	if (e.which == 16) {shiftPressed = true;} 
-	if (e.which == 56 && shiftPressed || e.which == 106) {
-		if (numberValues != null && histDispCont.value != null) {
-			equalFunc(e);		
-		}
-		if (dispCont.value == null || histDispCont.value == null) {
-			
-			dispCont.value = '';
-			histDispCont.value = '';
-		}
-		
-		histDispCont.value = dispCont.value + multiplySign.value;
-		operatorValues += multiplySign.value;
-		dispCont.value = '';
-		equalSign.disabled = false;
-	} else if (e.which == 54 && shiftPressed) {
-		if (numberValues != null && histDispCont.value != null) {
-
-			if (histDispCont.value.includes('fact')) {
-				histDispCont.value = '';
-			}
-			equalFunc(e);		
-		} 
-	
-		if (dispCont.value == null || histDispCont.value == null) {
-			
-			dispCont.value = '';
-			histDispCont.value = '';
-		}
-
-		histDispCont.value = dispCont.value + powerSign.value;
-		operatorValues += powerSign.value;
-		dispCont.value = '';
-		equalSign.disabled = false;
-	} else if (e.which == 61 || e.which == 187 && shiftPressed) {
-		if (numberValues != '' && histDispCont.value != '') {
-			equalFunc(e);		
-		} 
-	
-		if (dispCont.value == null || histDispCont.value == null) {
-			dispCont.value = '';
-			histDispCont.value = '';
-		}
-	
-		histDispCont.value = `${dispCont.value}${plusSign.value}`;
-		console.log(numberValues);
-		operatorValues = `${operatorValues}${plusSign.value}` 
-		dispCont.value = '';
-		equalSign.disabled = false;
-	}
-});
-
-document.addEventListener('keyup', function(e) {
-	if(e.which == 16) { shiftPressed = false}	
-})
+}
 
 function equalFunc(e) {
 		if (dispCont.value.indexOf('NaN') > -1){
@@ -322,10 +197,8 @@ function equalFunc(e) {
 	if (numberValues != '' && histDispCont.value != '') {
 	let result = operate(parseFloat(histDispCont.value), operatorValues.charAt(operatorValues.length-1), parseFloat(numberValues))
 	
-
 	let rounded = Math.round(result * 100000) / 100000;
 	dispCont.value = rounded;
-		console.log(numberValues);
 		histDispCont.value = '';
 
 		let splitted = dispCont.value.split('');
@@ -337,10 +210,6 @@ function equalFunc(e) {
 }
 }
 
-
-
-equalSign.addEventListener('click', equalFunc);
-
 function calcFactorial() {
 	if (dispCont.value == '') {
 		histDispCont.value = '';
@@ -348,37 +217,113 @@ function calcFactorial() {
 	numberValues = dispCont.value;
 	histDispCont.value = `fact(${dispCont.value})`; 
 	let result = factorial(numberValues);
-	//ispCont.value = +(result.toFixed(7));
 	dispCont.value = result
 	}
 }
-factorialOper.addEventListener('click', calcFactorial);
 
-decimalPoint.addEventListener('click', (e) => {
+function decimalPt() {
 	if (dispCont.value.indexOf('.') > -1){
 		return;
 	} else {
-		 dispCont.value += '.';
-		 //numberValues = null;
-		//histDispCont.value = '';
-		 
+		 dispCont.value += '.';		 
 	}
-})
+}
 
-    const CE = document.querySelector('.clear');
-    CE.addEventListener('click', () => {
+	function clearEntry() {
 		displayValues = '';
 		dispCont.value = '';
 		histDispCont.value = '';
-        // while (numberValues.length) {
-        //     numberValues.pop();
-		// }
 		numberValues = [];
 		operatorValues = []
-	})
+	}
 	
 	function backSpace() {
 		dispCont.value = dispCont.value.substring(0, dispCont.value.length-1)
 		numberValues = numberValues.substring(0, numberValues.length-1);
 	}
-	backspace.addEventListener('click', backSpace);
+
+	function operatorFunc(e) {
+		if (numberValues != '' && histDispCont.value != '') {
+			if (histDispCont.value.includes('fact')) {
+				histDispCont.value = '';
+			}
+			equalFunc(e);	
+		} 
+	
+		if (dispCont.value == null || histDispCont.value == null) {
+			e.target.value = ''
+			dispCont.value = '';
+			histDispCont.value = '';
+		}
+	
+		if (dispCont.value.indexOf('NaN') > -1 || histDispCont.value.indexOf('NaN') > -1){
+			dispCont.value = '';
+		}
+			histDispCont.value = `${dispCont.value}${e.target.value}`;
+			operatorValues = `${operatorValues}${e.target.value}` 
+			dispCont.value = '';
+	}
+
+	function add (...x) {
+		return result = x.reduce((a,b) => a += b);		
+	}
+	function subtract (...x) {
+		return result = x.reduce((a,b) => a -= b);
+	}
+	function multiply (...x) {
+		return result = x.reduce((a,b) => a *= b);
+	}
+	function divide(...x) {
+		return result = x.reduce((a,b) => a /= b);
+	}
+	function power(x,y) {
+		return x ** y;
+	}
+	function factorial(n) {
+		return n !== 0 ? n * factorial(n - 1) : 1;
+	}
+
+	function operate(x, oper, y) {
+		if (oper === '+') {
+			return add(x, y);
+		} else if (oper === '/' || oper === '÷') {
+			if (y === 0) {
+				return 'Can\'t divide by 0'
+			} else {
+			return divide(x, y);
+		}
+		} else if (oper === '*' || oper === 'x') {
+			return multiply(x, y);
+		} else if (oper === '-') {
+			return subtract(x, y);
+		} else if (oper === '^' || oper === 'x^') {
+			return power(x, y);
+		}
+	}
+
+	return {
+		init: () => {
+			calcBtns.forEach(btn => btn.addEventListener('click', (e) => {
+				displayValues = dispCont.value += e.target.value;
+				numberValues = dispCont.value
+			}));
+			
+			 allOperators.forEach(btn => btn.addEventListener('click', (e) => {
+				operatorFunc(e)
+				equalSign.disabled = false;	
+			}));
+			
+			backspace.addEventListener('click', backSpace);
+			CE.addEventListener('click', clearEntry);
+			factorialOper.addEventListener('click', calcFactorial);
+			decimalPoint.addEventListener('click', decimalPt);
+			equalSign.addEventListener('click', equalFunc);
+			document.addEventListener('keydown', keyDownEvents);
+			document.addEventListener('keydown', keyDownEventsWithShift);
+			document.addEventListener('keyup', function(e) {
+				if(e.which == 16) {shiftPressed = false}	
+			})
+			}
+	}
+})();
+Calculator.init();
